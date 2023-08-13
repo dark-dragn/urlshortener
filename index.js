e46cb395b0969154b93504908de47dd3eedcd579
Vasanth-Raja <darkdragn66@gmail.com>
2023-08-13 21:49:03 +0530

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
+console.log(process.env.DB_URL) 
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const {MongoClient}=require('mongodb');
 // Basic Configuration
+const cleint= new MongoClient(process.env.DB_URL);
+const db=cleint.db("urlshortener")
+const urls=db.collection("url")
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +24,11 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+app.post('/api/shorturl', function(req, res) {
+  console.log(req.body);
+  res.json({ greeting: 'hello API' });
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
