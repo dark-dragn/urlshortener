2262ec8bd8979c2486b613f28972af5a96cab854
Vasanth-Raja <darkdragn66@gmail.com>
2023-08-15 14:13:53 +0530

    final 1.0

--- index.js
+++ index.js
@@ -52,7 +52,11 @@ app.post('/api/shorturl', function(req, res) {
   )
  
 });
-
+app.get('/api/shorturl/:shorturl',async (req,res)=>{
+    const shorturl=req.params.shorturl;
+    const urlDoc= await urls.findOne({short_url:+shorturl})
+    res.redirect(urlDoc.url)
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
